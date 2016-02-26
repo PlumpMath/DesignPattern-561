94be1f3cdc4d848f091cc5c78e28794897bafea9
tangkun <kun.tang@qq.com>
2016-02-26 17:02:57 +0800

    。

--- src/单例模式Singleton/d2/Test.java
+++ src/单例模式Singleton/d2/Test.java
@@ -3,6 +3,8 @@ package 单例模式Singleton.d2;
 public class Test {
 
     public static void main(String[] args) {
+        RegSingleton s = new RegSingletonChild();
+        s.about();
         RegSingletonChild.getInstance().about();
     }
 
